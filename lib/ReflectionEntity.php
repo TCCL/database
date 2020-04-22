1f8ab3cc0b506cff28125511607bfbb011001cf7
Roger Gee <roger.gee@tulsalibrary.org>
2020-04-22 16:24:39 -0500

    ReflectionEntity: Add SQl-fragment generators

--- lib/ReflectionEntity.php
+++ lib/ReflectionEntity.php
@@ -65,6 +65,161 @@ abstract class ReflectionEntity extends Entity {
     }
 
     /**
+     * Creates an SQL fragment for filtering a SELECT query via a WHERE clause. The fragment
+     *
+     * @param array &$vars
+     *  Associative array that writes out variables required in the query
+     *  execution.
+     * @param array $filters
+     *  Associative array mapping field to filter value. If the value is NULL,
+     *  then the field is filtered using the expression IS NULL. If the value is
+     *  TRUE, then the field is filtered using the expression IS NOT NULL.
+     * @param string $tableAlias
+     *  The name alias to use for the table. If none is provided, then actual
+     *  table name is used.
+     * @param bool $strict
+     *  If strict, then non-existent fields throw an exception. Otherwise they
+     *  are included with no qualifying table name.
+     *
+     * @return string
+     *  Returns the generated SQL fragment.
+     */
+    public static function createFilterFragment(array &$vars,array $filters,$tableAlias = '',$strict = true) {
+        if (empty($filters)) {
+            return '';
+        }
+
+        $schema = self::getMetadata();
+
+        if (empty($tableAlias)) {
+            $tableAlias = $schema['table'];
+        }
+
+        $prefix = '';
+        for ($i = 0;$i < 4;++$i) {
+            $prefix .= chr(97 + rand(0,25));
+        }
+        $parts = [];
+        $n = 1;
+
+        foreach ($filters as $field => $value) {
+            if (array_key_exists($field,$schema['props'])) {
+                $ref = "`$tableAlias`.`{$schema['props'][$field]}`";
+            }
+            else if (array_key_exists($field,$schema['fields'])) {
+                $ref = "`$tableAlias`.`$field`";
+            }
+            else if (!$strict) {
+                $ref = $field;
+            }
+            else {
+                throw new Exception("Field '$field' does not exist in Entity schema");
+            }
+
+            if ($value === true) {
+                $parts[] = "$ref IS NOT NULL";
+            }
+            else if (is_null($value)) {
+                $parts[] = "$ref IS NULL";
+            }
+            else if (is_array($value)) {
+                if (empty($value)) {
+                    continue;
+                }
+
+                $preps = [];
+                foreach ($value as $val) {
+                    $vv = "f{$prefix}_" . $n++;
+                    $preps[] = ":$vv";
+                    $vars[$vv] = $val;
+                }
+                $preps = implode(',',$preps);
+                $parts[] = "$ref IN ($preps)";
+            }
+            else {
+                $vv = "f{$prefix}_" . $n++;
+                $parts[] = "$ref = :$vv";
+                $vars[$vv] = $value;
+            }
+        }
+
+        $fragment = implode(' AND ',$parts);
+
+        return $fragment;
+    }
+
+    /**
+     * Creates an ORDER BY SQL fragment.
+     *
+     * @param array $sortby
+     *  List of fields to sort within. An item in this list may be one of the
+     *  following:
+     *   - An associative entry mapping field/prop name to boolean indicating
+     *     whether the field is sorted in ascending order.
+     *   - An indexed entry where the value is the field/prop name. The field
+     *     will always be sorted in ascending order.
+     *   - An indexed entry where the value is 2-array; the first element is the
+     *     field/prop name and the second is a boolean indicating whether the
+     *     field is sorted in ascending order.
+     * @param string $tableAlias
+     *  The name alias to use for the table. If none is provided, then actual
+     *  table name is used.
+     * @param bool $strict
+     *  If strict, then non-existent fields throw an exception. Otherwise they
+     *  are included with no qualifying table name.
+     *
+     * @return string
+     *  Returns the generated SQL fragment.
+     */
+    public static function createSortByFragment(array $sortby,$tableAlias = '',$strict = true) {
+        if (empty($sortby)) {
+            return '';
+        }
+
+        $schema = self::getMetadata();
+
+        if (empty($tableAlias)) {
+            $tableAlias = $schema['table'];
+        }
+
+        $parts = [];
+
+        foreach ($sortby as $key => $value) {
+            if (is_array($value)) {
+                list($field,$asc) = $value;
+            }
+            else if (is_string($key)) {
+                $field = $key;
+                $asc = $value;
+            }
+            else {
+                $field = $value;
+                $asc = true;
+            }
+
+            if (array_key_exists($field,$schema['props'])) {
+                $ref = "`$tableAlias`.`{$schema['props'][$field]}`";
+            }
+            else if (array_key_exists($field,$schema['fields'])) {
+                $ref = "`$tableAlias`.`$field`";
+            }
+            else if (!$strict) {
+                $ref = $field;
+            }
+            else {
+                throw new Exception("Field '$field' does not exist in Entity schema");
+            }
+
+            $suffix = $asc ? 'ASC' : 'DESC';
+            $parts[] = "{$ref}{$suffix}";
+        }
+
+        $fragment = implode(', ',$parts);
+
+        return $fragment;
+    }
+
+    /**
      * Creates a new ReflectionEntity instance.
      *
      * @param DatabaseConnection $conn
@@ -152,10 +307,6 @@ abstract class ReflectionEntity extends Entity {
                     $filter = $tags['filter'];
 
                     $fields[$field] = null;
-                    if (isset($fields[$field])) {
-                        $fields[$field] = $filter($fields[$field]);
-                    }
-
                     $filters[$field] = $filter;
                 }
                 else {
